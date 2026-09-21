28d8332f336d3879fea91063edf732edbab40166
miau39 <166517798+miau39@users.noreply.github.com>
2026-09-21 12:32:45 +0200

    register script.js
    bo to jest rejestracja a nie login ups

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 przycisk.addEventListener("click", function () {
 
     if (haslo.value.length >= 8 && haslo.value.length <= 20 && login.value.length >=4 && login.value.length <=15 && parseInt(wiek.value) >=18 && regex.test(email.value)) {
-        komunikat.textContent = "Zalogowano poprawnie";
+        komunikat.textContent = "Zarejestrowano poprawnie";
     } else {
         komunikat.textContent = "Nieprawidłowy login lub hasło";
     }
